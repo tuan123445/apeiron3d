877780b956721cc60bc259399d590d42e37f067d
tuan123445 <anht91610@gmail.com>
2022-09-27 13:28:18 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup></script>
 
 <template>
   <RouterView />
